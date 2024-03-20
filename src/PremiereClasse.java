d99928191f9f0c79fd244255cbc349fdf7982403
Andreas MOUZONG <andreasmouzong@gmail.com>
2024-03-20 20:00:53 +0100

    Ajout d\'une Classe Java dans la branche Andreas

---
+++ src/PremiereClasse.java
@@ -0,0 +1,5 @@
+public class PremiereClasse {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans ma première Classe en java sur ma branche personnalisée!");
+    }
+}
