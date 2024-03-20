54168454990e9f255a7e2b04a0ce405148673664
Andreas MOUZONG <andreasmouzong@gmail.com>
2024-03-20 20:36:40 +0100

    Passage a la ligne de mon System Out Println

--- src/PremiereClasse.java
+++ src/PremiereClasse.java
@@ -1,5 +1,6 @@
 public class PremiereClasse {
     public static void main(String[] args) {
-        System.out.println("Bienvenue dans ma première Classe en java sur ma branche personnalisée!");
+        System.out.println("Bienvenue dans ma première Classe " +
+                "en java sur ma branche personnalisée!");
     }
 }
