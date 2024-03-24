9022d74683f5ef0edf77dc7b8de0759841bede6e
Steve Alex <stevy.alon@hotmal.com>
2024-03-24 17:26:28 +0100

    ajout de la classe Semaine.java

---
+++ src/Semaine.java
@@ -0,0 +1,10 @@
+public class Semaine {
+    private static String[] daysOfWeek = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+
+    public static void main(String[] args) {
+        for(String day: daysOfWeek){
+            System.out.println(day);
+        }
+    }
+
+}
