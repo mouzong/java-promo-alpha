b3ceff5e82633b7999fd5d3e50ad5a4cf4e42bb8
Yann Kwadjio <kwadjionry@gmail.com>
2024-03-24 14:42:53 +0100

    ajout class Menu, Semaine, et  yann

---
+++ src/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("Bienvenu au restaurant " +
+                "\n =========MENU PRINCIPAL=========" +
+                "\n 1. Jus de raisin---------1 000 FCFA" +
+                "\n 2. Jus d'Orange----------1 200 FCFA " +
+                "\n 3. Jus de Papaye---------1 500 FCFA" +
+                "\n 4. Eau minéral-----------500 FCFA " +
+                "\n 5. Jus de gingembre------2 000 FCFA " +
+                "\n 6. Sortir du programe " +
+                "\n\n Votre choix:");
+    }
+}
---
+++ src/Semaine.java
@@ -0,0 +1,5 @@
+public class Semaine {
+    public static void main(String[] args) {
+        System.out.println("Les 7 jours de la semaine sont: \n\t Lundi \n\t Mardi \n\t Mercredi \n\t Jeudi \n\t Vendredi \n\t Samedi \n\t Dimanche");
+    }
+}
