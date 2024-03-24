894a72a73819d940f89275e78be50ca51e377860
Andreas MOUZONG <andreasmouzong@gmail.com>
2024-03-24 13:39:58 +0100

    Ajout des variables et operateurs arithmetiques

--- src/PremiereClasse.java
+++
@@ -1,6 +0,0 @@
-public class PremiereClasse {
-    public static void main(String[] args) {
-        System.out.println("Bienvenue dans ma première Classe " +
-                "en java sur ma branche personnalisée!");
-    }
-}
--- src/TestPullClass.java
+++
@@ -1,5 +0,0 @@
-public class TestPullClass {
-    public static void main(String[] args) {
-        System.out.println("Je teste ma deuxieme classe pour le pull");
-    }
-}
