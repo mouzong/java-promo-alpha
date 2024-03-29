70fad11ad02ee467afe25115cef3c560c1131119
Steve Alex <stevy.alon@hotmail.com>
2024-03-29 12:18:45 +0100

    Refactoring de l'exercice 1

--- src/exercices/ExerciceOne.java
+++ src/exercices/ExerciceOne.java
@@ -6,31 +6,34 @@ import java.util.Scanner;
 public class ExerciceOne {
     boolean yes = false;
 
-    private void calculate(int number){
-
-        System.out.println("============ Multiplication table of " + number + " =========");
+    private void printMultiplicationTableOf(int number){
         for (int i = 0; i <= 10; i++) {
             int result = number * i;
             System.out.println("                   "+number + " x" + " " + i + " = " + result);
         }
-        System.out.println("\n");
     }
 
     public void run() {
         do{
             int number = 0;
-            System.out.println("Enter a number to display its multiplication table...");
-            Scanner reader = new Scanner(System.in);try {
-            number = reader.nextInt();
-            yes = true;
-            }catch(InputMismatchException e){
+            System.out.println("\n"+"Enter a number to display its multiplication table, or any character to exit.");
+            Scanner reader = new Scanner(System.in);
+            try {
+                number = Integer.parseInt(reader.next());
+                if(number >= 0) {
+                    yes = true;
+                }
+            }catch(InputMismatchException | NumberFormatException e){
                 yes = false;
             }
             if(yes){
-                calculate(number);
+                System.out.println("============ Multiplication table of " + number + " =========");
+                printMultiplicationTableOf(number);
+                System.out.println("\n");
+            }else{
+                System.out.println("You did not enter a number! The program will exit. Thank you and good bye!");
             }
         }
         while(yes);
-        System.out.println("Thank you an good bye!");
     }
 }
