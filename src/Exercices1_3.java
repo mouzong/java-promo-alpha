d44f75a0fb99bbb3eb84a25297ac7971db980cfa
yannkwadjio <kwadjionry@gmail.com>
2024-03-30 12:23:47 +0100

    Modification des exercices en cours

--- src/Exercices1_3.java
+++ src/Exercices1_3.java
@@ -2,22 +2,18 @@ public class Exercices1_3 {
 
     public static void main(String[] args) {
 
-        if (args.length == 1) {   //Vérifie si le nombre d'argument est différent de 1;
+        //Vérifie si le nombre d'argument est différent de 1;
 
-            int nbreChoisi = Integer.parseInt(args[0]); //conversion argument en nombre
-            //Exercice 1: Multiplication d'un nombre de notre choix
+        int nbreChoisi = 6; //choix fait
+        //Exercice 1: Multiplication d'un nombre de notre choix
 
-            System.out.println("Exercice 1\n==========Table de multiplication de " + nbreChoisi + "==========\n");
-            for (int j = 1; j <= 12; j++) {
-                System.out.println("\t\t\t" + nbreChoisi + "x" + j + "=" + nbreChoisi * j + "\n");
-            }
-
-
-        } else {
-            System.out.println("Un seul paramètre est requis pour cette opération");
+        System.out.println("Exercice 1\n==========Table de multiplication de " + nbreChoisi + "==========\n");
+        for (int j = 1; j <= 12; j++) {
+            System.out.println("\t\t\t" + nbreChoisi + "x" + j + "=" + nbreChoisi * j + "\n");
         }
 
-        //Exercice 2 Multiplication des nombre de 1 à 9
+
+        //Exercice 2 Multiplication des nombres de 1 à 9
 
         final int nbreDepart = 1; //Affectation du nombre de départ (constante)
         System.out.println("Exercice 2\n==========Table de multiplication de 1 à 9==========\n");
@@ -30,10 +26,10 @@ public class Exercices1_3 {
 
 
         //bloc 4 à 6
-        for (int j = 1; j <= 12; j++){
+        for (int j = 1; j <= 12; j++) {
             System.out.println(
                     "|\t" + (nbreDepart + 3) + "x" + j + "=" + (nbreDepart + 3) * j + "\t|\t" + (nbreDepart + 4) + "x" + j + "=" + (nbreDepart + 4) * j + "\t|\t" + (nbreDepart + 5) + "x" + j + "=" + (nbreDepart + 5) * j + "\t|");
-    }
+        }
         System.out.println("-------------------------------------------------");
 
 
@@ -43,7 +39,14 @@ public class Exercices1_3 {
         }
         System.out.println("-------------------------------------------------");
 
+
+        //Exercice 3 Ecrire un programme java qui calcule et affiche la moyenne de trois nombre entier au hasard
+        int nbre1= (int) (Math.random()*1001);
+        int nbre2=(int)(Math.random()*1001);
+        int nbre3=(int)(Math.random()*1001);
+        System.out.println("La moyenne entre "+nbre1+","+nbre2+","+nbre3+" est de "+(nbre1+nbre2+nbre3)/3);
+
     }
-    }
+}
 
 
--- src/Exercices8.java
+++ src/Exercices8.java
@@ -1,22 +1,28 @@
 public class Exercices8 {
     public static void main(String[] args) {
 
-        //Exercice 8 Ecrire un programme java qui affiche la table de multiplication d'un nombre pair. Le programme doit controler et afficher uniquement les multiples differents du nombre considéré.
+        //Exercice 8 Ecrire un programme java qui affiche la table de multiplication d'un nombre pair.
+        // Le programme doit controler et afficher uniquement les multiples differents du nombre considéré.
 
-if(args.length==1){
-    int nbreChoisi=Integer.parseInt(args[0]);
-    if(nbreChoisi%2==0){
-        System.out.println("Exercice 8\n ==========Table de multiplication de "+nbreChoisi+"==========\n");
-               for(int j=1;j<=12;j++) {
-                   System.out.println("\t\t\t"+nbreChoisi+"x"+j+"="+nbreChoisi*j+"\n");
+        if (args.length == 1) {
+            int nbreChoisi = Integer.parseInt(args[0]);
+            if (nbreChoisi % 2 == 0) {
+                System.out.println("Exercice 8\n ==========Table de multiplication de " + nbreChoisi + "==========\n");
+                for (int j = 1; j <= 12; j++) {
+                    if (nbreChoisi != (nbreChoisi * j) ) {
+                        System.out.println("\t\t\t" + nbreChoisi + "x" + j + "=" + nbreChoisi * j + "\n");
+                    } else {
+
+                    }
+
+                }
+
+            } else {
+                System.out.println("Bien vouloir renseigner un nombre pair");
+            }
+
+        } else {
+            System.out.println("Un seul paramètre est requis");
         }
-
-    }else{
-        System.out.println("Bien vouloir renseigner un nombre pair");
     }
-
-}else{
-    System.out.println("Un seul paramètre est requis");
-}
-         }
 }
