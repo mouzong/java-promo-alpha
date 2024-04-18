8fbe18e0584d2fee1007be8dda540d1190d0f2e1
yannkwadjio <kwadjionry@gmail.com>
2024-04-18 19:15:10 +0200

    commit-180420241815

---
+++ src/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Ajout_user user1=new Ajout_user();
+      Scanner sc=new Scanner(System.in);
+        String[] tab=user1.user(sc);
+        System.out.println(tab[0]);
+    }
+}
