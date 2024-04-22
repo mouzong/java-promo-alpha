c3edfadcb81932de8d1222b405a2d35bc00ff115
yannkwadjio <kwadjionry@gmail.com>
2024-04-22 05:57:21 +0100

    commit-220420240557

--- src/poo/Main_poo.java
+++
@@ -1,8 +0,0 @@
-package poo;
-
-public class Main_poo {
-    public static void main(String[] args) {
-        Utilisateur user1=new Utilisateur("KWADJIO");
-        System.out.println(user1.getNom());
-    }
-}
